39080f7506fa80d5eb61d86db4a41a517b51d3d5
Sjaak Luthart <mail@sjaakluthart.nl>
2017-03-04 16:33:36 +0100

    Removed phone and mail buttons.

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,6 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
-import Mailto from 'react-mailto';
 import logo from '../assets/images/logo.svg';
-import phone from '../assets/images/phone.svg';
-import mail from '../assets/images/mail.svg';
 import { languageSet } from '../actions';
 
 function Header(props) {
@@ -11,8 +8,6 @@ function Header(props) {
     <header>
       <img src={logo} alt="logo" />
       <section>
-        <a href="tel:+31 6 47 25 07 01"><img src={phone} alt="phone" /></a>
-        <Mailto email="dj.bruin@gmail.com" obfuscate><img src={mail} alt="mail" /></Mailto>
         <p onClick={() => props.languageSet('nl')}>NL</p>
         <p onClick={() => props.languageSet('de')}>DE</p>
       </section>
