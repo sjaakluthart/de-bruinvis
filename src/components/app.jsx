20a8aeaa8f113a5b9d02506827a62e0c00b236a5
Sjaak Luthart <mail@sjaakluthart.nl>
2020-11-23 14:57:08 +0100

    Added react helmet.

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Helmet from 'react-helmet';
+
 import Header from './header';
 import Slider from './slider';
 import About from './about';
@@ -11,6 +13,7 @@ import CopyRight from './copyright';
 function App() {
   return (
     <main>
+      <Helmet title="Vakantiehuisje De Bruinvis" description="Vakantiehuisje De Bruinvis staat in het rustige havenplaatsje Oudeschild. Het is een zeer goed gerestaureerd, autheniek, vrijstaand en sfeervol vissershuisje voor 4 a 5 personen." />
       <Header />
       <Slider />
       <About />
